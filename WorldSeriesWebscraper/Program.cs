ccc8058cbdd887e9d7bdb958fb252e348919b967
Robert Fahey <rf.businessacademy@gmail.com>
2018-11-08 10:00:41 -0500

    Created world series webscraper project

---
+++ WorldSeriesWebscraper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldSeriesWebscraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new HttpClient();
+            var result=client.GetStringAsync("https://en.wikipedia.org/wiki/List_of_World_Series_champions#Winners").Result;
+            
+        }
+    }
+}
+//https://en.wikipedia.org/wiki/List_of_World_Series_champions#Winners
